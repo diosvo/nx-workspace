d0abb7b494e32d8daffaf3f926f26810d2b0e9a8
Dios Vo <vtmn1212@gmail.com>
2021-05-01 15:38:54 +0700

    Create Widgets Service

--- fem-production/libs/api-interfaces/src/lib/api-interfaces.ts
+++ fem-production/libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,7 +1,3 @@
-export interface Message {
-  message: string;
-}
-
 export class Widget {
 
   constructor(public id: string | null, public title: string, public description: string) { }
--- fem-production/libs/core-data/src/lib/services/widgets/widgets.service.ts
+++ fem-production/libs/core-data/src/lib/services/widgets/widgets.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Widget } from '@fem-production/api-interfaces';
+import { Observable } from 'rxjs';
+
+const API_ENDPOINT = 'http://localhost:3000/'
 
 @Injectable({
   providedIn: 'root'
 })
-export class WidgetsService {
 
-  constructor() { }
+export class WidgetsService {
+  model = 'widgets';
+
+  constructor(private http: HttpClient) { }
+
+  all(): Observable<Widget[]> {
+    return this.http.get<Widget[]>(this.getURL());
+  }
+
+  find(id: string): Observable<Widget> {
+    return this.http.get<Widget>(this.getUrlWithId(id));
+  }
+
+  create(widget: Widget): Observable<Widget> {
+    return this.http.post<Widget>(this.getURL(), widget);
+  }
+
+  update(widget: Widget): Observable<Widget> {
+    return this.http.put<Widget>(this.getUrlWithId(widget.id), widget);
+  }
+
+  delete(widget: Widget): Observable<Widget> {
+    return this.http.delete<Widget>(this.getUrlWithId(widget.id));
+  }
+
+  private getURL(): string {
+    return API_ENDPOINT + this.model;
+  }
+
+  private getUrlWithId(id: string): string {
+    return this.getURL() + `/${id}`;
+  }
 }
