bb6bdf6679d5b29d24968143848e842985a5a03e
Dios Vo <vtmn1212@gmail.com>
2021-05-02 14:48:49 +0700

    Configuring Nest to Mock a Service. Remove run mock API.

--- fem-production/apps/api/src/widgets/widgets.service.ts
+++ fem-production/apps/api/src/widgets/widgets.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateWidgetDto } from './dto/create-widget.dto';
 import { UpdateWidgetDto } from './dto/update-widget.dto';
+import { Widget } from '@fem-production/api-interfaces';
 
 @Injectable()
 export class WidgetsService {
+
+  widgets: Array<Widget> = [
+    {id:'1', title:'Title 01', description: 'Pending'},
+    {id:'2', title:'Title 02', description: 'Pending'},
+    {id:'3', title:'Title 03', description: 'Pending'},
+  ];
+
   create(createWidgetDto: CreateWidgetDto) {
     return 'This action adds a new widget';
   }
 
   findAll() {
-    return `This action returns all widgets`;
+    return this.widgets;
   }
 
   findOne(id: number) {
